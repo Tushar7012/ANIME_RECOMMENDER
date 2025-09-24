74691a4df066ab4230e7804cdd55cdea55807731
Tushar <td220627@gmail.com>
2025-09-24 10:51:46 +0530

    Recommender Added  **

---
+++ src/recommender.py
@@ -0,0 +1,22 @@
+from langchain.chains import RetrievalQA
+from langchain_groq import ChatGroq
+from src.prompt_template import get_anime_prompt
+
+"""For the Recommender System the Columns --> Genre + synopsis are combined to give the LLM more context about the anime"""
+
+class AnimeRecommender:
+    def __init__(self, retriever,api_key:str,model_name:str):
+        self.llm = ChatGroq(api_key=api_key, model_name=model_name,temperature=0)
+        self.prompt = get_anime_prompt()
+
+        self.qa_chain = RetrievalQA.from_chain_type(
+            llm = self.llm,
+            chain_type="stuff",
+            retriever = retriever,
+            return_source_documents = True,
+            chain_type_kwargs = {"prompt": self.prompt}
+        )
+
+    def get_recommendations(self, query:str):
+        result = self.qa_chain({"query": query})
+        return result['result']
